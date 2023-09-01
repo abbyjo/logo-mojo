61f8db6d000f41f70de875ae25f1767896ddb2fd
abbyjo <abbyjo225@gmail.com>
2023-09-01 18:31:21 -0400

    Added prompts

---
+++ index.js
@@ -0,0 +1,26 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'What text would you like in your logo? (Max 3 characters)'
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'What color would you like your text to be? (Color or Hex)'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape would you like your logo to have?',
+        choices: ['Circle', 'Triangle', 'Square']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: "What color would you like your logo's shape to be? (Color or Hex)"
+    }
+]
